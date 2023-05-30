c253a3d4e867ef16aca7222c3c929e694d5b7c8b
John <johntay33lor@gmail.com>
2023-05-30 16:29:04 -0400

    Initial commit

--- src/com/algorithms_bigOnotation/CountingSort.java
+++ src/com/algorithms_bigOnotation/CountingSort.java
@@ -1,2 +1,38 @@
-package com.algorithms_bigOnotation;public class CountingSort {
+package com.algorithms_bigOnotation;
+
+public class CountingSort {
+    public static String countingSort (String arr) {
+        //Find the maximum element in the input array
+        int max = 0;
+        for (int i = 0; i < arr.length(); i++) {
+            max = Math.max(max, arr.charAt(i) - 'a');
+        }
+
+        //Create a count array to store the frequency of each character
+        int[] count = new int[max + 1];
+        for (int i = 0; i < arr.length(); i++) {
+            count[arr.charAt(i) - 'a']++;
+        }
+
+        //Modify the count array to store the cumulative frequency
+        for (int i = 1; i <= max; i++) {
+            count[i] += count[i - 1];
+        }
+
+        //Create a sorted output array
+        char[] output = new char[arr.length()];
+        for (int i = arr.length() - 1; i >= 0; i--) {
+            output[count[arr.charAt(i) - 'a'] - 1] = arr.charAt(i);
+        }
+
+        //Convert the output array to a string and return it
+        return new String(output);
+    }
+
+    //Testing countingSort
+    public static void main(String[] args) {
+        String input = "gfedcba";
+        String sorted = countingSort(input);
+        System.out.println(sorted);
+    }
 }
--- src/com/algorithms_bigOnotation/QuickSort.java
+++ src/com/algorithms_bigOnotation/QuickSort.java
@@ -1,2 +1,58 @@
-package com.algorithms_bigOnotation;public class QuickSort {
+package com.algorithms_bigOnotation;
+
+public class QuickSort {
+    // Partitioning the array and returning the pivot index
+    public static int partition (int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    // Recursive function to perform quicksort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+
+            // Sort the left partition
+            quickSort(arr, low, pivotIndex - 1);
+            // Sort the right partition
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    // Swap two elements in the array
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // Testing quickSort
+    public static void main(String[] args) {
+        int[] arr = {6, 2, 7, 13, 1, 5, 18, 3};
+
+        System.out.println("Array before sorting:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // Sort the array using quickSort
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Array after sorting:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
